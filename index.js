18f0b66d5617b89de80de8bd1450be4d88e3740b
Mankai06 <99220041682@klu.ac.in>
2025-08-13 00:04:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+/*var sidenav = document.querySelector(".side-navbar");
+
+function showNavbar()
+{
+    sidenav.style.display="block"
+}
+function closeNavbar()
+{
+    sidenav.style.display="none"
+     
+}*/
+var sidenav = document.querySelector(".side-navbar");
+
+function showNavbar() {
+    sidenav.style.left = "0"; 
+}
+
+function closeNavbar() {
+    sidenav.style.left = "-60%"; 
+}
